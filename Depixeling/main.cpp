fbc766983aa8e947170db7dd8740261b215766ae
seanzzz <Sean@WL-26-221.CINE.Claremont.Edu>
2015-04-14 15:29:39 -0700

    Initial Commit

---
+++ Depixeling/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Depixeling
+//
+//  Created by Huangjian Zhu on 4/14/15.
+//  Copyright (c) 2015 Huangjian Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
